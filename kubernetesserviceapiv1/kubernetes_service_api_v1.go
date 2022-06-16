2c92705175c7037ccc089100438500c948b90bf6
bhpratt <bhpratt@ncsu.edu>
2022-06-16 15:34:47 -0400

    updated wording for description

--- kubernetesserviceapiv1/kubernetes_service_api_v1.go
+++ kubernetesserviceapiv1/kubernetes_service_api_v1.go
@@ -28208,7 +28208,7 @@ type MultishiftGetController struct {
 	// CommonClusterCACertRotation CA cert rotation properties.
 	CaCertRotationStatus *CommonClusterCACertRotation `json:"caCertRotationStatus,omitempty"`
 
-	// CoreosEnabled Optional: Enable Red Hat CoreOS features within the Satellite location.
+	// CoreosEnabled Optional: Returns whether Red Hat CoreOS features are enabled within the Satellite location.
 	CoreosEnabled *bool `json:"coreos_enabled,omitempty"`
 
 	// COSBucket Optional: IBM Cloud Object Storage bucket configuration details.
